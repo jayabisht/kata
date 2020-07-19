4f7b38bb55842c74c658367b822627c9ed10ad31
Jaya Bisht <jaya.bisht@credivia.com>
2020-07-20 01:04:27 +0530

    Created StringCalculator and StringCalculatorTest.

---
+++ src/main/java/com/kata/code/StringCalculator.java
@@ -0,0 +1,9 @@
+package com.kata.code;
+
+public class StringCalculator {
+
+	public Integer Add(String string) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/kata/test/StringCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.kata.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.kata.code.StringCalculator;
+
+class StringCalculatorTest {
+
+	@Test
+	void testEmptyString() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(0,calculator.Add(""));
+	}
+
+}
