76a1536ff7371d5cc3b4815a2d442d509807053a
Jaya Bisht <jayabisht123@gmail.com>
2020-07-21 15:27:53 +0530

    Support dynamic delimiters for Add method.

--- src/main/java/com/kata/code/StringCalculator.java
+++ src/main/java/com/kata/code/StringCalculator.java
@@ -1,21 +1,62 @@
 package com.kata.code;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
 	public Integer Add(String numbers) {
+		
 		if(numbers.equals(""))
 			return 0;
-		else if(!numbers.contains(",") && !numbers.contains("\n"))
-			return Integer.valueOf(numbers);
-		else {
+		
+		String defaultDelimiter = getDefaultDelimiter(numbers);
+		
+		int sum = 0;
+		if(defaultDelimiter == null) {
+		
+			if(!numbers.contains(",") && !numbers.contains("\n"))
+				return Integer.valueOf(numbers);
+			
 			String[] numberArray = numbers.split(",|\n");
-			int sum = 0;
 			for(int i =0;i<numberArray.length;i++) {
 				sum += Integer.valueOf(numberArray[i]);
 			}
-			
-			return sum;
 		}
+		else {
+			String exactNumber = numbers.replaceFirst("//(.*?)\n", "");
+			
+			if(exactNumber.equals(""))
+				return 0;
+			
+			if(!exactNumber.contains(defaultDelimiter))
+				return Integer.valueOf(exactNumber);
+			
+			String[] numberArray = exactNumber.split(Pattern.quote(defaultDelimiter));
+			for(int i =0;i<numberArray.length;i++) {
+				sum += Integer.valueOf(numberArray[i]);
+			}
+		}
+		
+		return sum;
+		
+	}
+	
+	/**
+	 * Get Default Delimiter according to the pattern '//[delimiter]\n[numbers]' if exists
+	 * else return null
+	 * @param numbers
+	 * @return
+	 */
+	private String getDefaultDelimiter(String numbers) {
+		if(numbers.startsWith("//")) {
+			Pattern pattern = Pattern.compile("//(.*?)\n");
+			Matcher matcher = pattern.matcher(numbers);
+			while (matcher.find()) {
+			    return matcher.group(1);
+			}
+		}
+		return null;
 	}
 
 }
--- src/test/java/com/kata/test/StringCalculatorTest.java
+++ src/test/java/com/kata/test/StringCalculatorTest.java
@@ -7,28 +7,27 @@ import org.junit.jupiter.api.Test;
 import com.kata.code.StringCalculator;
 
 class StringCalculatorTest {
+	
+	StringCalculator calculator = new StringCalculator();
 
 	@Test
 	void testEmptyString() {
-		StringCalculator calculator = new StringCalculator();
+		
 		assertEquals(0,calculator.Add(""));
 	}
 	
 	@Test
 	void testOneNumber() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(10,calculator.Add("10"));
 	}
 	
 	@Test
 	void testTwoNumbers() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(3,calculator.Add("1,2"));
 	}
 	
 	@Test
 	void testMutipleNumbers() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(19,calculator.Add("1,8,10"));
 		assertEquals(27,calculator.Add("1,8,10,1,7"));
 		assertEquals(176,calculator.Add("1,8,10,1,7,10,40,99"));
@@ -36,10 +35,18 @@ class StringCalculatorTest {
 	
 	@Test
 	void testNewLinesWithNumbers() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(19,calculator.Add("1\n8\n10"));
 		assertEquals(27,calculator.Add("1,8,10\n1,7"));
 		assertEquals(176,calculator.Add("1,8\n10,1,7\n10,40\n99"));
+	}
+	
+	@Test
+	void testDynamicDelimiters() {
+		assertEquals(0,calculator.Add("//;\n"));
+		assertEquals(1,calculator.Add("//;\n1"));
+		assertEquals(3,calculator.Add("//;\n1;2"));
+		assertEquals(27,calculator.Add("//>\n1>8>10>1>7"));
+		assertEquals(176,calculator.Add("//++\n1++8++10++1++7++10++40++99"));
 	}
 
 }
