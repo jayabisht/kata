ec2437cd82aa4d26bbd13cdc1b662059d5f792c3
Jaya Bisht <jayabisht123@gmail.com>
2020-07-23 11:48:56 +0530

    Allow multiple delimiters.

--- src/main/java/com/kata/code/StringCalculator.java
+++ src/main/java/com/kata/code/StringCalculator.java
@@ -1,81 +1,91 @@
 package com.kata.code;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
 	
-	public static final int MAX_NUMBER = 1000;
-	public static final String DELIMITER_FORMAT = "//\\[?(.*?)\\]?\n";
+	public final int MAX_NUMBER = 1000;
+	public final String DELIMITER_FORMAT = "\\[?([^\\]]*)\\]?";			
+	public final String[] DEFAULT_DELIMITERS = {",","\n"};
 
 	public Integer Add(String numbers) throws Exception {
 		
+		// Return 0 for empty string
 		if(numbers.equals(""))
 			return 0;
 		
-		String defaultDelimiter = getDefaultDelimiter(numbers);
+		List<String> delimiters = getDelimiters(numbers);
 		
 		int sum = 0;
-		if(defaultDelimiter == null) {
-		
-			if(!numbers.contains(",") && !numbers.contains("\n")) {
-				checkForNegativeNumbers(new String[] {numbers});
-				if(Integer.valueOf(numbers)>MAX_NUMBER)
-					return 0;
-				return Integer.valueOf(numbers);
-			}
+		// Extract exact numbers from input
+		if(!delimiters.contains("\n")) {
+			String[] parts = numbers.split("\n");
+			if(parts.length>1)
+				numbers = parts[1];
+			else
+				numbers = "";
 			
-			String[] numberArray = numbers.split(",|\n");
-			checkForNegativeNumbers(numberArray);
-			for(int i =0;i<numberArray.length;i++) {
-				if(Integer.valueOf(numberArray[i])>MAX_NUMBER)
-					continue;
-				sum += Integer.valueOf(numberArray[i]);
-			}
 		}
-		else {
-			String exactNumber = numbers.replaceFirst(DELIMITER_FORMAT, "");
-			
-			if(exactNumber.equals(""))
+		
+		// Return 0 for empty string
+		if(numbers.equals(""))
+			return 0;
+		final String exactNumber = numbers;
+		// If there is no delimiter in input, return the input as number
+		if(!delimiters.stream().anyMatch(d->exactNumber.contains(d))) {
+			checkForNegativeNumbers(new String[] {numbers});
+			if(Integer.valueOf(numbers)>MAX_NUMBER)
 				return 0;
-			
-			if(!exactNumber.contains(defaultDelimiter)) {
-				checkForNegativeNumbers(new String[] {exactNumber});
-				if(Integer.valueOf(exactNumber)>MAX_NUMBER)
-					return 0;
-				return Integer.valueOf(exactNumber);
-			}
-			
-			String[] numberArray = exactNumber.split(Pattern.quote(defaultDelimiter));
-			checkForNegativeNumbers(numberArray);
-			for(int i =0;i<numberArray.length;i++) {
-				if(Integer.valueOf(numberArray[i])>MAX_NUMBER)
-					continue;
-				sum += Integer.valueOf(numberArray[i]);;
-			}
+			return Integer.valueOf(numbers);
 		}
 		
+		// Split numbers according to delimiters and calculate sum
+		String splitPattern = "";
+		for(String d: delimiters)
+			splitPattern += Pattern.quote(d) + "|";
+		splitPattern = splitPattern.substring(0, splitPattern.length()-1);
+		
+		String[] numberArray = numbers.split(splitPattern);
+		checkForNegativeNumbers(numberArray);
+		for(int i =0;i<numberArray.length;i++) {
+			if(Integer.valueOf(numberArray[i])>MAX_NUMBER)
+				continue;
+			sum += Integer.valueOf(numberArray[i]);
+		}		
 		return sum;
 		
 	}
 	
 	/**
-	 * Get Default Delimiter according to the pattern '//[delimiter]\n[numbers]' if exists
-	 * else return null
+	 * Get Delimiters according to the pattern if exists or default ones
 	 * @param numbers
 	 * @return
 	 */
-	private String getDefaultDelimiter(String numbers) {
+	private List<String> getDelimiters(String numbers) {
+		List<String> delimiters = new ArrayList<String>();
 		if(numbers.startsWith("//")) {
+			numbers = numbers.replaceFirst("//", "");
+			numbers = numbers.split("\n")[0];
 			Pattern pattern = Pattern.compile(DELIMITER_FORMAT);
 			Matcher matcher = pattern.matcher(numbers);
 			while (matcher.find()) {
-			    return matcher.group(1);
+				if(!matcher.group(1).equals(""))
+					delimiters.add(matcher.group(1));
 			}
+			return delimiters;
 		}
-		return null;
+		return Arrays.asList(DEFAULT_DELIMITERS);
 	}
 	
+	/**
+	 * Check for negative numbers in the given Array and throw Exception if there is any negative number.
+	 * @param numberArray
+	 * @throws Exception
+	 */
 	private void checkForNegativeNumbers(String numberArray[]) throws Exception {
 		String negativeNumbers = "";
 		for(String number : numberArray) {
@@ -85,7 +95,7 @@ public class StringCalculator {
 				
 		}
 		if(!negativeNumbers.equals(""))
-			throw new Exception("negatives not allowed "+negativeNumbers.substring(0, negativeNumbers.length()-1));
+			throw new Exception("negatives not allowed " + negativeNumbers.substring(0, negativeNumbers.length()-1));
 	}
 
 }
--- src/test/java/com/kata/test/StringCalculatorTest.java
+++ src/test/java/com/kata/test/StringCalculatorTest.java
@@ -101,5 +101,14 @@ class StringCalculatorTest {
 		assertEquals(27,calculator.Add("//[>>>]\n1>>>8>>>10>>>1>>>7"));
 		assertEquals(176,calculator.Add("//[++]\n1++8++10++1++7++10++40++99"));
 	}
+	
+	@Test
+	void testMultipleDelimiters() throws Exception {
+		assertEquals(0,calculator.Add("//[;][&]\n"));
+		assertEquals(1,calculator.Add("//[-]\n1"));
+		assertEquals(3,calculator.Add("//[*]\n1*2"));
+		assertEquals(27,calculator.Add("//[_][;][%]\n1_8%10;1_7"));
+		assertEquals(176,calculator.Add("//[@][&]\n1@8@10&1@7&10&40&99"));
+	}
 
 }
